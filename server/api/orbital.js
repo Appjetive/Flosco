b7ced3dd38b75de8289ecea6956897afa201fb5f
Alexey <alexey@rootstack.com>
2019-10-20 11:34:44 -0500

    Adding geting api json data

--- server/api/orbital.js
+++ server/api/orbital.js
@@ -2,9 +2,19 @@ const express = require('express');
 const path = require('path');
 const router = express.Router();
 const fs = require('fs');
+const axios = require('axios');
 const _ = require('lodash/core');
 
 router.get("/", (req, res, next) => {
+  checkApi();
+  res.json({error:"Not found"});
+});
+
+router.get("/url", (req, res, next) => {
+  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
+
+function checkApi() {
   let date = new Date();
   let dir = "scrap";
   let directoryPath = path.join(__dirname, dir);
@@ -16,23 +26,27 @@ router.get("/", (req, res, next) => {
           return console.log('Unable to scan directory: ' + err);
       }
       if (_.isEmpty(files)) {
-        fs.writeFile(directoryPath + '/' + date.toISOString() + '.json','12345', function (err) {
+        const getScrapMetal = () => {
+          try {
+            return axios.get('https://worldwind.arc.nasa.gov/spacebirds/data/TLE.json')
+          } catch (error) {
+            console.error(error)
+          }
+        }
+        const scrapMetal = getScrapMetal();
+        fs.writeFile(directoryPath + '/' + date.toISOString() + '.json', scrapMetal, function (err) {
           if (err) throw err;
           console.log('File is created successfully.');
         });
-      }
-      files.forEach(function (file) {
+      } else {
+        files.forEach(function (file) {
+          let fileName = file.split(".");
+          let fileDate = Date.parse(fileName[0]);
           console.log(file); 
-      });
+        });
+      }
     });
   }
-
-  // fs.writeFile('<fileName>',,callbackFunction);
-  res.json({error:"Not found"});
-});
-
-router.get("/url", (req, res, next) => {
-  res.json(["Tony","Lisa","Michael","Ginger","Food"]);
-});
+}
 
 module.exports = router;
